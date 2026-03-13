5ace8237eedea1c9f7514f5e6df8988ab12f5e89
Pedro <pedrofbks@gmail.com>
2026-03-13 15:05:19 -0300

    feat: implement process builder for remote command execution and task reporting

---
+++ sentinel-agent/agent/agent/src/test/java/com/sentinel/agent/SentinelAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
